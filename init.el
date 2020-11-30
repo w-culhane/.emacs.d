c13f638257fbac9a7e99b5cd3e361680ac57e23b
William Culhane <sxroka@gmail.com>
2020-11-30 15:02:17 -0600

    Set basic options from doom config

--- init.el
+++ init.el
@@ -2,8 +2,25 @@
 (require 'borg)
 (borg-initialize)
 
+(require 'evil)
+(evil-mode 1)
+
 (with-eval-after-load 'magit
 		      (magit-add-section-hook 'magit-status-sections-hook
 					      'magit-insert-modules
 					      'magit-insert-stashes
 					      'append))
+
+; (load-theme 'quasi-monochrome t)
+
+(setq user-full-name "William Culhane"
+      user-mail-address "sxroka@gmail.com")
+
+(setq org-directory "~/Documents/org"
+      deft-directory "~/Documents/org")
+
+(setq org-agenda-files (list org-directory))
+
+(setq org-log-done 'time)
+
+(setq display-line-numbers-type t)
