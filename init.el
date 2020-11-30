8a1bb3c4cbae5c86a0c4fda46fdf7b84e57d3581
William Culhane <sxroka@gmail.com>
2020-11-30 17:22:41 -0600

    Clean up init.el

--- init.el
+++ init.el
@@ -1,33 +1,38 @@
+; Borg initialization
 (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
 (require 'borg)
 (borg-initialize)
 
+; Plugins
 (require 'evil)
 (evil-mode 1)
 
+(require 'ivy)
 (ivy-mode 1)
 
+(require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+(require 'magit)
 (with-eval-after-load 'magit
 		      (magit-add-section-hook 'magit-status-sections-hook
 					      'magit-insert-modules
 					      'magit-insert-stashes
 					      'append))
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-
+(require 'quasi-monochrome)
 (load-theme 'quasi-monochrome t)
 
 (setq user-full-name "William Culhane"
       user-mail-address "sxroka@gmail.com")
 
-(setq org-directory "~/Documents/org"
-      deft-directory "~/Documents/org")
-
+; Org setup
+(setq org-directory "~/Documents/org")
 (setq org-agenda-files (list org-directory))
-
 (setq org-log-done 'time)
 
+; UI
 (setq display-line-numbers-type t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
