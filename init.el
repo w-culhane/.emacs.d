033090424606096a7c229dd6553466b81cac615a
William Culhane <sxroka@gmail.com>
2020-11-30 12:19:20 -0600

    Add magit hook to init.el

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
 (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
 (require 'borg)
 (borg-initialize)
+
+(with-eval-after-load 'magit
+		      (magit-add-section-hook 'magit-status-sections-hook
+					      'magit-insert-modules
+					      'magit-insert-stashes
+					      'append))
