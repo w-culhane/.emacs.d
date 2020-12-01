5bf47c1ce541695a49a0547905ffc8f85cab4da5
William Culhane <sxroka@gmail.com>
2020-11-30 20:44:55 -0600

    Tweak UI settings

--- init.el
+++ init.el
@@ -56,10 +56,14 @@
 (setq org-log-done 'time)
 
 ; UI
-(setq display-line-numbers-type t)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(set-fringe-mode 10)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(tooltip-mode -1)
 
 (setq inhibit-splash-screen t)
