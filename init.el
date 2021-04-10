19c643d4a88514a07360f8159a66d9cfe546bf3e
William Culhane <will@culhane.top>
2021-04-10 13:00:14 -0600

    emacs: Remove borg

--- init.el
+++ init.el
@@ -3,6 +3,8 @@
 (setq user-full-name "William Culhane"
       user-mail-address "sxroka@gmail.com")
 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 ; UI
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -29,113 +31,128 @@
       org-agenda-start-with-log-mode t
       org-agenda-start-on-weekday 0)
 
-; Borg initialization
-(add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
-(require 'borg)
-(borg-initialize)
+; Package management
+(require 'package)
 
-(eval-when-compile (require 'use-package))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 (use-package auto-compile
-  :config
-  (auto-compile-on-load-mode)
-  (auto-compile-on-save-mode))
+	     :config
+	     (auto-compile-on-load-mode)
+	     (auto-compile-on-save-mode))
+
+(use-package auto-package-update
+	     :custom
+	     (auto-package-update-interval 7)
+	     (auto-package-update-prompt-before-update t)
+	     :config
+	     (auto-package-update-maybe)
+	     (auto-package-update-at-time "09:00"))
 
 ; Plugins
 (use-package quasi-monochrome-theme
-  :config (load-theme 'quasi-monochrome t))
+	     :config (load-theme 'quasi-monochrome t))
 
 (use-package evil
-  :init
-  (setq evil-want-keybinding nil
-	evil-want-integration t
-	evil-want-C-u-scroll t
-	evil-undo-system 'undo-redo)
-  :config (evil-mode 1))
+	     :init
+	     (setq evil-want-keybinding nil
+		   evil-want-integration t
+		   evil-want-C-u-scroll t
+		   evil-undo-system 'undo-redo)
+	     :config (evil-mode 1))
 
 (use-package evil-collection
-  :after evil
-  :config (evil-collection-init))
+	     :after evil
+	     :config (evil-collection-init))
 
 (use-package evil-vimish-fold
-  :after evil)
+	     :after evil)
 
 (use-package evil-nerd-commenter
-  :after evil
-  :config (evilnc-default-hotkeys nil t))
+	     :after evil
+	     :config (evilnc-default-hotkeys nil t))
 
 (use-package evil-surround
-  :after evil
-  :config (global-evil-surround-mode 1))
+	     :after evil
+	     :config (global-evil-surround-mode 1))
 
 (use-package evil-lion
-  :after evil
-  :config (evil-lion-mode))
+	     :after evil
+	     :config (evil-lion-mode))
 
 (use-package evil-exchange
-  :after evil
-  :config (evil-exchange-cx-install))
+	     :after evil
+	     :config (evil-exchange-cx-install))
 
 (use-package ivy
-  :config (ivy-mode 1))
+	     :config (ivy-mode 1))
 
 (use-package ivy-rich
-  :after ivy
-  :config (ivy-rich-mode 1))
+	     :after ivy
+	     :config (ivy-rich-mode 1))
 
 (use-package counsel
-  :config (counsel-mode 1))
+	     :config (counsel-mode 1))
 
 (use-package which-key
-  :init (setq which-key-idle-delay 0.5)
-  :config (which-key-mode))
+	     :init (setq which-key-idle-delay 0.5)
+	     :config (which-key-mode))
 
 (use-package helpful
-  :config
-  (global-set-key (kbd "C-h f") #'helpful-callable)
-  (global-set-key (kbd "C-h v") #'helpful-variable)
-  (global-set-key (kbd "C-h k") #'helpful-key))
+	     :config
+	     (global-set-key (kbd "C-h f") #'helpful-callable)
+	     (global-set-key (kbd "C-h v") #'helpful-variable)
+	     (global-set-key (kbd "C-h k") #'helpful-key))
 
 (use-package flycheck
-  :config
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+	     :config
+	     (add-hook 'after-init-hook #'global-flycheck-mode))
 
 (use-package toc-org
-  :init
-  (add-hook 'org-mode-hook 'toc-org-mode)
-  (add-hook 'markdown-mode-hook 'toc-org-mode))
+	     :init
+	     (add-hook 'org-mode-hook 'toc-org-mode)
+	     (add-hook 'markdown-mode-hook 'toc-org-mode))
 
 (use-package magit
-  :config
-  (magit-add-section-hook 'magit-status-sections-hook
-			  'magit-insert-modules
-			  'magit-insert-stashes
-			  'append))
+	     :config
+	     (magit-add-section-hook 'magit-status-sections-hook
+				     'magit-insert-modules
+				     'magit-insert-stashes
+				     'append))
 
 (use-package projectile
-  :init
-  (setq projectile-project-search-path '("~/.emacs.d" "~/depot" "~/Documents"))
-  :config
-  (projectile-mode +1))
+	     :init
+	     (setq projectile-project-search-path '("~/.emacs.d" "~/depot" "~/Documents"))
+	     :config
+	     (projectile-mode +1))
 
 (use-package general
-  :config
-  (general-create-definer general/main
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
+	     :config
+	     (general-create-definer general/main
+				     :keymaps '(normal insert visual emacs)
+				     :prefix "SPC"
+				     :global-prefix "C-SPC")
 
-  (general/main
-    "j"  'swiper
-    "na" 'org-agenda
-    "m"  (general-simulate-key "C-c")
-    "x"  (general-simulate-key "C-x")
-    "h"  (general-simulate-key "C-h")
-    "cc" 'evilnc-comment-or-uncomment-lines
-    "cu" 'uncomment-region
-    "ct" 'evilnc-toggle-invert-comment-line-by-line
-    "ff" 'find-file
-    "bb" 'counsel-switch-buffer
-    "pf" 'projectile-find-file
-    "pp" 'projectile-switch-project
-    "gg" 'magit))
+	     (general/main
+	       "j"  'swiper
+	       "na" 'org-agenda
+	       "m"  (general-simulate-key "C-c")
+	       "x"  (general-simulate-key "C-x")
+	       "h"  (general-simulate-key "C-h")
+	       "cc" 'evilnc-comment-or-uncomment-lines
+	       "cu" 'uncomment-region
+	       "ct" 'evilnc-toggle-invert-comment-line-by-line
+	       "ff" 'find-file
+	       "bb" 'counsel-switch-buffer
+	       "pf" 'projectile-find-file
+	       "pp" 'projectile-switch-project
+	       "gg" 'magit))
