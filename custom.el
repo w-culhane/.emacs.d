74cf79f9038f7e0cab0075e3d7792b3b8087991a
William Culhane <will@culhane.top>
2022-04-22 14:40:12 -0600

    Apply automatic custom updates

--- custom.el
+++ custom.el
@@ -10,7 +10,8 @@
  '(notmuch-fcc-dirs "top/Sent")
  '(package-selected-packages
    '(elpher auctex outshine org-msg projectile-ripgrep lsp-ivy lsp-mode notmuch general projectile magit toc-org flycheck helpful which-key counsel ivy-rich ivy evil-exchange evil-lion evil-surround evil-nerd-commenter evil-vimish-fold evil-collection evil quasi-monochrome-theme auto-package-update auto-compile use-package))
- '(send-mail-function 'sendmail-send-it))
+ '(send-mail-function 'sendmail-send-it)
+ '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
