19d303ad3964d7724807df36fcbecbfb4fd70ef1
William Culhane <will@culhane.top>
2023-04-22 14:04:36 -0600

    Update packages

--- init.el
+++ init.el
@@ -65,12 +65,12 @@
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -109,7 +109,6 @@
 	     :host github
 	     :repo "mickeynp/ligature.el")
   :config
-  (setq ligature-composition-table nil)
   (ligature-set-ligatures 'prog-mode '("<---" "<--"  "<<-" "<-" "->" "-->" "--->" "<->" "<-->" "<--->" "<---->" "<!--"
 				       "<==" "<===" "<=" "=>" "=>>" "==>" "===>" ">=" "<=>" "<==>" "<===>" "<====>" "<!---"
 				       "<~~" "<~" "~>" "~~>" "::" ":::" "==" "!=" "===" "!=="
